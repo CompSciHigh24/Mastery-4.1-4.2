c61c8dcbffbd9e64ec163339df5ea15e2f108374
CompSciHigh24 <csh-github@compscihigh.org>
2025-02-21 12:52:30 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+/**
+ * STEP 0: npm i express
+ */
+
+/**
+ * STEP 1: Set up a "public" folder and add static files into it
+ */
+
+
+/**
+ * STEP 2: Go into dance.html and make sure that it links to both the css and js files
+ */
+
+/**
+ * STEP 3: Add static file middleware. 
+ */
+
+
+/**
+ * STEP 4: Create a route for "/dance" that serves the dance.html file
+ */
+
+
+app.listen(3000, () => {
+    console.log(`Server is running`);
+});
